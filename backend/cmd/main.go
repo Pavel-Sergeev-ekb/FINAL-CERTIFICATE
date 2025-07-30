a0560b6d4511786d4a04c60f694a5839004817c8
Pavel Sergeev <slampam@yandex.ru>
2025-07-30 16:53:56 +0500

    fix db2

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,13 +5,13 @@ import (
 	"log"
 	"os"
 
-	"github.com/Pavel-Sergeev-ekb/FINAL-CERTIFICATE/backend/internal/database"
+	"github.com/Pavel-Sergeev-ekb/FINAL-CERTIFICATE/backend/internal/data"
 	"github.com/Pavel-Sergeev-ekb/FINAL-CERTIFICATE/backend/internal/server"
 )
 
 func main() {
 
-	db, err := sql.Open("sqlite3", database.GetDBPath())
+	db, err := sql.Open("sqlite3", data.GetDBPath())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,7 +20,7 @@ func main() {
 	if err := db.Ping(); err != nil {
 		log.Fatal("failed to connect db:", err)
 	}
-	if err := database.InitDB(db); err != nil {
+	if err := data.InitDB(db); err != nil {
 		log.Fatal(err)
 	}
 
